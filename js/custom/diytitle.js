14d5ced09495f4218220d4a4aaafbb3792bec488
CapyPola <x18550647894@gmail.com>
2026-01-27 22:22:23 +0800

    Site updated: 2026-01-27 22:22:22

---
+++ js/custom/diytitle.js
@@ -0,0 +1,18 @@
+//动态标题
+var OriginTitile = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        //离开当前页面时标签显示内容
+        document.title = 'w(ﾟДﾟ)w 不要走！再看看嘛！😢';
+        clearTimeout(titleTime);
+    }
+    else {
+        //返回当前页面时标签显示内容
+        document.title = '♪(^∇^*)欢迎回来！😘' + OriginTitile;
+        //两秒后变回正常标题
+        titleTime = setTimeout(function () {
+            document.title = OriginTitile;
+        }, 2000);
+    }
+});
